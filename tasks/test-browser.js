7fcbe62daf2a0186d51517522b712b03f939933d
AdelJS <bigfanjs@gmail.com>
2016-10-12 23:29:15 +0100

    fixing the conf path in test-browser task

--- tasks/test-browser.js
+++ tasks/test-browser.js
@@ -1,10 +1,11 @@
 import karma from 'karma';
+import path from 'path';
 
 export const Server = karma.Server;
 export default function () {
   return callback => {
     (new Server({
-      configFile: 'karma.conf.js'
+      configFile: path.join(__dirname, '../karma.conf.js')
     }, callback)).start();
   };
 }
