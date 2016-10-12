8ad1c6aa3410075cb024346531449bd682b0c190
AdelJS <bigfanjs@gmail.com>
2016-10-12 22:33:12 +0100

    updating gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,11 +3,11 @@ import gulp from 'gulp';
 import bundle from './tasks/bundle';
 import browserSync from './tasks/browser-sync';
 import nodemon from './tasks/nodemon';
-import test_browser from './tasks/test-browser';
+import testBrowser from './tasks/test-browser';
 
 gulp.task('bundle', bundle());
 gulp.task('nodemon', nodemon());
 gulp.task('browserSync', ['nodemon'], browserSync());
-gulp.task('test-browser', test_browser());
+gulp.task('test-browser', testBrowser());
 
 gulp.task('default', ['browserSync', 'bundle']);
