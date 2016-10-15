0c51aa8aee163b64381849578c5a1e21aad1adae
AdelJS <bigfanjs@gmail.com>
2016-10-15 14:03:50 +0100

    creating a layout reusable components

---
+++ public/javascripts/lib/layout.js
@@ -0,0 +1,50 @@
+import Backbone from 'backbone';
+import $ from 'jquery';
+import forEach from 'lodash/forEach';
+import ModelView from './model-view';
+import Region from './region';
+
+const
+  closeRegions = function ( regions ) {
+    forEach(regions, ( name, region ) => {
+      if ( region && region.remove ) {
+        region.remove();
+      }
+    });
+  },
+  configRegions = function () {
+    if ( !this._regions ) {
+      this._regions = {};
+    }
+
+    forEach(this.regions, ( name, value ) => {
+      this._regions[ name ] = Region.setup({ elem: $( value ) });
+    });
+  };
+
+export default ModelView.extend({
+  render() {
+    closeRegions( this.regions );
+
+    // this gonna get changed!
+    ModelVie.prototype.render.call( this );
+
+    configRegions.call( this );
+  },
+  // gets the region by name.
+  getRegion( name ) {
+    var region;
+
+    if ( name === null ) {
+      throw TypeError('No name property passed!');
+    }
+
+    if ( !regions.hasOwnProperty( name ) ) {
+      throw TypeError(`${ name } region doesn't exist in the layout!`);
+    }
+
+    region = regions[ name ];
+
+    return region;
+  }
+});
