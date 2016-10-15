367ac3a4b8fd5e61ba3d08889c2fd101c244cce8
AdelJS <bigfanjs@gmail.com>
2016-10-15 14:09:14 +0100

    creating a model view reusable component

---
+++ public/javascripts/lib/model-view.js
@@ -0,0 +1,25 @@
+import Backbone from 'backbone';
+
+// This is responsible for rendring models
+const render = function () {
+  const
+    model = this.model ? this.model.toJSON() : {},
+    html = this.template( model );
+
+  /* replace the contents of the element by
+    a peice of HTML which is genreated by
+    the template. */
+  this.$el.html( html );
+
+  /* if the model exists, we need to stick the model
+    to the view for two-way data bindings. */
+  if ( this.model ) this.stickit();
+
+  if ( this.onRender ) this.onRender();
+
+  return this;
+};
+
+export default Backbone.View.extend({
+  render
+});
