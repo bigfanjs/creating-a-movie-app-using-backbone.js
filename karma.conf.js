d8bf68eaa4d0632057a44d2209bce068ab1b6c7d
AdelJS <bigfanjs@gmail.com>
2016-10-22 18:03:15 +0100

    fixing an error in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        '../stylesheets/mocha.css',
+        '../stylesheets/vendors/mocha.css',
         'lib/**/*.js',
         'apps/**/*.js',
         'test/**/*.js'
