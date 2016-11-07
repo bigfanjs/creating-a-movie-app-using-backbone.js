49c16c1ff948f30923baf82073183f695e7ecd32
AdelJS <bigfanjs@gmail.com>
2016-11-07 23:55:37 +0000

    creating the movies sub-app router

---
+++ public/javascripts/apps/movies/router.js
@@ -0,0 +1,20 @@
+import Backbone from 'backbone';
+import MoviesApp from './index';
+import App from '../../app';
+
+export default Backbone.Router.extend({
+  initialize: function () {
+    this.app = App.start( MoviesApp );
+  },
+  routes: {
+    'movies': 'displayMovies',
+    'movies/page/:page': 'displayMovies',
+    'movies/view/:id': 'viewMovie'
+  },
+  displayMovies: function ( id = 1 ) {
+    this.app.viewList(id > 0 ? id : 1);
+  },
+  viewMovie: function ( id ) {
+    this.app.viewMovie( id );
+  }
+});
