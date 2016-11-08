ba4f28fee5ae6893ab2844689941a98bf423fc54
AdelJS <bigfanjs@gmail.com>
2016-11-08 15:03:31 +0000

    creating a movie model in mongoose

---
+++ models/movie.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://127.0.0.1/movies');
+
+const schema = new mongoose.Schema({
+  title: String,
+  type: String,
+  runningTime: String,
+  releaseDate: String,
+  featureSong: String,
+  story: String,
+  directore: String,
+  boxOffice: String,
+  narrator: String,
+  country: String,
+  language: String,
+  cast: [{
+    actor: String,
+    character: String,
+    avatar: {
+      name: String,
+      path: String
+    }
+  }],
+  starring: [{
+    actor: String,
+    character: String,
+    avatar: {
+      name: String,
+      path: String
+    }
+  }],
+  cover: {
+    name: String,
+    path: String
+  }
+});
+
+module.exports = mongoose.model('movie', schema);
