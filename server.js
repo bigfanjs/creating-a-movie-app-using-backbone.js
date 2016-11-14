17c9563a25330fd672b94f242339382238737f3e
AdelJS <bigfanjs@gmail.com>
2016-11-14 23:23:15 +0000

    updating server.js

--- server.js
+++ server.js
@@ -1,24 +1,46 @@
-import path from 'path';
-import express from 'express';
-import bodyParser from 'body-parser';
-import favicon from 'server-favicon';
-import logger from 'morgan';
+'use strict';
 
-// importing routes:
-import showMovies from './routes/show-movies';
-import viewMovie from './routes/view-movie';
+const
+  path = require('path'),
+  express = require('express'),
+  session = require('express-session'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan');
 
+// requiring routes:
+const
+  movies = require('./routes/movies'),
+  login = require('./routes/login');
+
+// initiating express:
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'jade');
 
 app.use(favicon('./public/favicon.ico'));
 app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, './public')));
 app.use(bodyParser.json());
+app.use(cookieParser('What should I say?'));
+app.use(session());
+app.use(express.static(path.join(__dirname, './public')));
+app.use('/admin/movies', movies.authenticate);
 
-app.get('/api/movies/', showMovies);
-app.get('/api/movies/:id', viewMovie);
+app.get('/api/movies/', movies.showMovies);
+app.get('/api/movies/:id', movies.viewMovie);
+
+// administration:
+app.get('/admin/login/', login.form);
+app.post('/admin/login/', login.submit);
+app.get('/admin/logout/', login.logout);
+
+app.get('/admin/movies/:id', movies.viewMovie);
+app.get('/admin/movies/', movies.showMovies);
+app.post('/admin/movies/', movies.createMovie);
+app.put('/admin/movies/:id', movies.updateMovie);
+app.delete('/admin/movies/:id', movies.deleteMovie);
 
 app.listen(3000, function () {
   console.log('Listening on port 3000');
