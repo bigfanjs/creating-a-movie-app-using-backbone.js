17c67faa4eb4a33412ce086a5e7a26590e3656ad
AdelJS <bigfanjs@gmail.com>
2016-11-17 22:24:59 +0000

    adding update, create and authenticate routes

--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,14 @@
 
 const Movie = require('../models/movie');
 
+exports.authenticate = function (req, res, next) {
+  if (req.session.uid) {
+    return next();
+  } else {
+    res.redirect('/admin/login');
+  }
+};
+
 exports.showMovies = function ( req, res, next ) {
   Movie.find((err, movies) => {
     if ( err ) return next( err );
@@ -16,6 +24,27 @@ exports.viewMovie = function ( req, res, next ) {
 
     res.json( movie );
   });
+};
+
+exports.createMovie = function (req, res, next) {
+  Movie.create((err, movie) => {
+    if ( err ) return next( err );
+
+    res.status( 201 ).json( movie );
+  });
+};
+
+exports.updateMovie = function (req, res, next) {
+  Movie.update(
+    { _id: req.params.id },
+    req.body,
+    { multi: false },
+    (err, movie) => {
+      if ( err ) return next( err );
+
+      res.json( movie );
+    }
+  );
 };
 
 exports.deleteMovie = function ( req, res, next ) {
