975f60684f1518120440a83dd9a793620474f791
AdelJS <bigfanjs@gmail.com>
2016-11-23 22:57:35 +0000

    updating css min gulp task [renaming all files to the root stylesheets folder]

--- tasks/css-min.js
+++ tasks/css-min.js
@@ -1,18 +1,22 @@
 import gulp from 'gulp';
 import minify from 'gulp-cssnano';
 import filter from 'gulp-filter';
-import concat from 'gulp-concat';
+import rename from 'gulp-rename';
 
 export default function () {
   return () => {
     const f = filter([
-      'public/stylesheets/styles.css'
+      'public/stylesheets/styles.css',
+      'public/stylesheets/mocha.css'
     ], { restore: true });
 
     gulp.src('public/stylesheets/**/*.css')
       .pipe(f)
       .pipe(minify())
       .pipe(f.restore)
+      .pipe(rename(path => {
+        path.dirname = './';
+      }))
       .pipe(gulp.dest('../build/public/stylesheets'));
   };
 }
