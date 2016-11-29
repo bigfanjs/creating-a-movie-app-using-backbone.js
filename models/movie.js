8fb112e70c239f76559b99f7aed99e83b52a5de6
AdelJS <bigfanjs@gmail.com>
2016-11-29 13:07:52 +0000

    updating the movie model [exporting a function which used by the models provide]

--- models/movie.js
+++ models/movie.js
@@ -1,41 +1,39 @@
 'use strict';
 
-const mongoose = require('mongoose');
-
-mongoose.createConnection('mongodb://127.0.0.1/movies');
-
-const schema = new mongoose.Schema({
-  title: String,
-  type: String,
-  runningTime: String,
-  releaseDate: String,
-  featureSong: String,
-  story: String,
-  directore: String,
-  boxOffice: String,
-  narrator: String,
-  country: String,
-  language: String,
-  cast: [{
-    actor: String,
-    character: String,
-    avatar: {
+module.exports = function (mongoose, db) {
+  const schema = new mongoose.Schema({
+    title: String,
+    type: String,
+    runningTime: String,
+    releaseDate: String,
+    featureSong: String,
+    story: String,
+    directore: String,
+    boxOffice: String,
+    narrator: String,
+    country: String,
+    language: String,
+    cast: [{
+      actor: String,
+      character: String,
+      avatar: {
+        name: String,
+        path: String
+      }
+    }],
+    starring: [{
+      actor: String,
+      character: String,
+      avatar: {
+        name: String,
+        path: String
+      }
+    }],
+    cover: {
       name: String,
       path: String
     }
-  }],
-  starring: [{
-    actor: String,
-    character: String,
-    avatar: {
-      name: String,
-      path: String
-    }
-  }],
-  cover: {
-    name: String,
-    path: String
-  }
-});
+  });
 
-module.exports = mongoose.model('movie', schema);
+  return db.model('movie', schema);
+};
