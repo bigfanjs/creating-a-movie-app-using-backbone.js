9e863479c937838b9f751a998db090ebf278fa5c
AdelJS <bigfanjs@gmail.com>
2016-11-30 22:16:16 +0000

    updating the movies route

--- routes/movies.js
+++ routes/movies.js
@@ -2,47 +2,51 @@
 
 const Movie = require('../models/movie');
 
-exports.showMovies = function ( req, res, next ) {
+const send404 = function (res, err) {
+  res.status(404).end(err);
+};
+
+exports.showMovies = function (req, res) {
   Movie.find((err, movies) => {
-    if ( err ) return next( err );
+    if ( err ) { return send404(res, err); }
 
-    res.json( movies );
+    res.status(200).json( movies );
   });
 };
 
-exports.viewMovie = function ( req, res, next ) {
+exports.viewMovie = function (req, res) {
   Movie.find({ _id: req.params.id }, ( err, movie ) => {
-    if ( err ) return next( err );
+    if ( err ) { return send404(res, err); }
 
-    res.json( movie );
+    res.status(200).json( movie );
   });
 };
 
-exports.createMovie = function (req, res, next) {
+exports.createMovie = function (req, res) {
   Movie.create((err, movie) => {
-    if ( err ) return next( err );
+    if ( err ) { return send404(res, err); }
 
     res.status( 201 ).json( movie );
   });
 };
 
-exports.updateMovie = function (req, res, next) {
+exports.updateMovie = function (req, res) {
   Movie.update(
     { _id: req.params.id },
     req.body,
     { multi: false },
     (err, movie) => {
-      if ( err ) return next( err );
+      if ( err ) { return send404(res, err); }
 
-      res.json( movie );
+      res.status(201).json( movie );
     }
   );
 };
 
-exports.deleteMovie = function ( req, res, next ) {
-  Movie.remove({ _id: req.params.id }, ( err, movie ) => {
-    if ( err ) return next( err );
+exports.deleteMovie = function (req, res) {
+  Movie.remove({ _id: req.params.id }, (err, movie) => {
+    if ( err ) { return send404(res, err); }
 
-    res.json( movie );
+    res.status(204).json( movie );
   });
 };
