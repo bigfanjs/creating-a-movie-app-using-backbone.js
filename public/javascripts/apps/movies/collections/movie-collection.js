d65cd1f11c7b7cedb2f815b43bbc9cec9f2da937
AdelJS <bigfanjs@gmail.com>
2016-12-05 23:41:26 +0000

    adding the leading slash to the api/movies route to make an absolute request to the server

--- public/javascripts/apps/movies/collections/movie-collection.js
+++ public/javascripts/apps/movies/collections/movie-collection.js
@@ -3,6 +3,6 @@ import MovieModel from '../models/movie-model';
 
 // a collection for movie models.
 export default Backbone.Collection.extend({
-  url: 'api/movies',
+  url: '/api/movies',
   model: MovieModel
 });
