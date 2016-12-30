741a782ccb200f92417a96306f27ec109d75db8a
Adel Jojo <bigfanjs@gmail.com>
2016-12-30 12:25:07 +0000

    updating the base-router lib component

--- public/javascripts/lib/base-router.js
+++ public/javascripts/lib/base-router.js
@@ -5,6 +5,10 @@ import Session from '';
 export default Backbone.Router.extend({
   route: function (route, name, callback) {
     if (!_isRegExp(route)) route = _routeToRegExp(route);
+    if (_isFunction(name)) {
+      callback = name;
+      name = '';
+    }
     if (!callback) callback = this[name];
 
     if (requiresAuth.contains( route )) {
@@ -16,5 +20,17 @@ export default Backbone.Router.extend({
         }
       });
     }
+
+    Backbone.history.route(route, fragment => {
+      const args = this._extractParameters(route, fragment);
+
+      if (this.execute(callback, args, name) !== false) {
+        this.trigger.apply(this, ['route' + name].concat(args));
+        this.trigger('route', name, args);
+        Backbone.history.trigger('route', router, name, args);
+      }
+    });
+
+    return this;
   }
 });
