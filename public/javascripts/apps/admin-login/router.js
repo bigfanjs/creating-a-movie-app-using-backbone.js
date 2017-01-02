837819350f08657fc557e2bea5a03df17db799e9
Adel Jojo <bigfanjs@gmail.com>
2017-01-02 12:44:08 +0000

    extending the the baseRouter and moving in the before's admin/login

--- public/javascripts/apps/admin-login/router.js
+++ public/javascripts/apps/admin-login/router.js
@@ -1,13 +1,26 @@
 import Backbone from 'backbone';
 import App from '../../app';
 import loginApp from './index.js';
+import BaseRouter from '../../lib/base-router';
 
-export default Backbone.Router.extend({
+export default BaseRouter.extend({
   initialize: function () {
     this.app = App.start( loginApp );
   },
   routes: {
     'admin/login': 'login'
+  },
+  before: {
+    'admin/login': function () {
+      return new Promise((resolve, reject) => {
+        this.porformAuth()
+          .done(() => {
+            this.navigate('admin/dashboard', true);
+            reject();
+          })
+          .fail(() => { resolve(); });
+      });
+    }
   },
   login: function () {
     this.app.viewForm();
