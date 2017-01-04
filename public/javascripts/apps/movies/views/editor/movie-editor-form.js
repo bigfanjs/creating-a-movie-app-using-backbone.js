a9b99ab7e30a5bcc66d342f9929943cf394e7a72
Adel Jojo <bigfanjs@gmail.com>
2017-01-04 12:16:36 +0000

    adding a className prop to the movie editor form view

--- public/javascripts/apps/movies/views/editor/movie-editor-form.js
+++ public/javascripts/apps/movies/views/editor/movie-editor-form.js
@@ -3,7 +3,7 @@ import template from '../../templates/editor/movie-editor-form.pug';
 
 export default ModelView.extend({
   template,
-  className: '',
+  className: 'col-xs-12 col-sm-8 col-md-9',
   events: {
     'click #cancel': 'cancel',
     'click #save': 'save'
