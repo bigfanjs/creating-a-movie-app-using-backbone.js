1249105c9530ea3d19cffdc548b4a3f6b60a0554
Adel Jojo <bigfanjs@gmail.com>
2017-01-05 22:37:25 +0000

    updating the movie list view

--- public/javascripts/apps/movies/views/list/movie-list-view.js
+++ public/javascripts/apps/movies/views/list/movie-list-view.js
@@ -2,9 +2,6 @@ import CollectionView from '../../../../lib/collection-view';
 import ItemView from './movie-list-item-view';
 
 export default CollectionView.extend({
-  initialize( options ) {
-    CollectionView.prototype.initialize.call( this, options );
-  },
   className: 'movie-list-view col-xs-8 panel panel-primary',
-  MovieItem: ItemView
+  ItemView
 });
