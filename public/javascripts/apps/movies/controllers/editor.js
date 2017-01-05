85f27a8e49fe01d7ba480910b0c90276fd23f678
Adel Jojo <bigfanjs@gmail.com>
2017-01-05 22:30:24 +0000

    updating the editor controller [controlling the cast and starring views]

--- public/javascripts/apps/movies/controllers/editor.js
+++ public/javascripts/apps/movies/controllers/editor.js
@@ -2,6 +2,10 @@ import Backbone from 'backbone';
 import MovieEditorLayout from '../views/editor/movie-editor-layout';
 import MovieEditorForm from '../views/editor/movie-editor-form';
 import MovieEditorPreview from '../views/editor/movie-editor-preview';
+import MovieEditorCastView from '../views/editor/movie-editor-cast-view';
+import MovieEditorStarringView from '../views/editor/movie-editor-starring-view';
+import MovieCharCollection from '../collections/movie-char-collection';
+import bind from 'lodash/bind';
 import App from '../../../app';
 
 const
@@ -46,6 +50,20 @@ const
   },
   cancel = function () {
     App.router.navigate('admin/dashboard', true);
+  },
+  addCastItem = function () {
+    console.log('Shit!');
+    this.castCollection.add({});
+  },
+  addStarringItem = function () {
+    this.starringCollection.add({});
+  },
+  deleteCastItem = function ( actor ) {
+    console.log('Shit!');
+    this.castCollection.remove( actor );
+  },
+  deleteStarringItem = function ( actor ) {
+    this.starringCollection.remove( actor );
   };
 
 export default {
@@ -58,16 +76,36 @@ export default {
   },
   view: function ( model ) {
     const
+      cast = model.get('cast') || [],
+      starring = model.get('starring') || [];
+
+    this.castCollection = new MovieCharCollection( cast );
+    this.starringCollection = new MovieCharCollection( starring );
+
+    const
       layout = new MovieEditorLayout(),
-      form = new MovieEditorForm(),
-      preview = new MovieEditorPreview();
+      form = new MovieEditorForm({ model }),
+      preview = new MovieEditorPreview({ model }),
+      castList = new MovieEditorCastView({
+        collection: this.castCollection
+      }),
+      starringList = new MovieEditorStarringView({
+        collection: this.starringCollection
+      });
 
     this.region.show( layout );
     layout.getRegion('form').show( form );
     layout.getRegion('preview').show( preview );
+    form.getRegion('cast').show( castList );
+    form.getRegion('starring').show( starringList );
 
-    this.listenTo(form, 'save', save);
-    this.listenTo(form, 'cancel', cancel);
+    this.listenTo(form, 'form:save', bind(save, this));
+    this.listenTo(form, 'form:cancel', cancel);
+    this.listenTo(form, 'cast:add', bind(addCastItem, this));
+    this.listenTo(form, 'starring:add', bind(addStarringItem, this));
+
+    this.listenTo(castList, 'item:actor:delete', bind(deleteCastItem, this));
+    this.listenTo(starringList, 'item:actor:delete', bind(deleteStarringItem, this));
 
     this.listenTo(preview, 'avatar:selected', handleCoverSelect);
   },
