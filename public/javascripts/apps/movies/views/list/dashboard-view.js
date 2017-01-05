058c46380f1e4ce7ce42d6b056114de9b8481549
Adel Jojo <bigfanjs@gmail.com>
2017-01-05 22:34:52 +0000

    updating the dashboard view

--- public/javascripts/apps/movies/views/list/dashboard-view.js
+++ public/javascripts/apps/movies/views/list/dashboard-view.js
@@ -3,9 +3,6 @@ import CollectionView from '../../../../lib/collection-view';
 import ItemView from './dashboard-item-view';
 
 export default CollectionView.extend({
-  initialize( options ) {
-    CollectionView.prototype.initialize.call( this, options );
-  },
   tagName: 'tbody',
-  MovieItem: ItemView
+  ItemView
 });
