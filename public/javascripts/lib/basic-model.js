37a8b0e67db72e0740258444eef3c7cf691df3b2
Adel Jojo <bigfanjs@gmail.com>
2017-01-06 17:00:59 +0000

    creating a basic model lib component

---
+++ public/javascripts/lib/basic-model.js
@@ -0,0 +1,31 @@
+import Backbone from 'backbone';
+
+export default Backbone.Model.extend({
+  upload: function (blob, type, options) {
+    const form = new FormData();
+
+    form.append(type, blob);
+
+    $.ajax({
+      url: `/api/movies/${ this.get('_id') }/${ type }`,
+      method: 'GET',
+      data: form,
+      success: options.success,
+      error: options.error,
+      cache: false,
+      contentType: false,
+      processData: false,
+      xhr: function () {
+        const xhr = new XMLHttpRequest();
+
+        xhr.upload.addEventListener('progress', event => {
+          const { length, uploaded } = event;
+
+          options.progress(length, uploaded, uploaded/length);
+        }, false);
+
+        return xhr;
+      }
+    });
+  }
+});
