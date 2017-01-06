12d2423b7b4810b33d27b47b3db15f5aff439256
Adel Jojo <bigfanjs@gmail.com>
2017-01-06 17:03:54 +0000

    extending the basic model in the character model and adding an uploadAvatar method

--- public/javascripts/apps/movies/models/movie-character-model.js
+++ public/javascripts/apps/movies/models/movie-character-model.js
@@ -1,9 +1,12 @@
-import Backbone from 'backbone';
+import BasicModel from '../../../lib/basic-model';
 
-export default Backbone.Model.extend({
+export default BasicModel.extend({
   defaults: {
     actor: '',
     character: '',
     avatar: null
+  },
+  uploadAvatar: function (blob, options) {
+    this.upload(blob, 'avatar', options);
   }
 });
