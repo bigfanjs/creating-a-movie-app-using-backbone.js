f4fd8621cc0c7b98d086daa4b4367d3a80d495b5
Adel Jojo <bigfanjs@gmail.com>
2017-01-06 22:33:35 +0000

    handling cast item avatar selection

--- public/javascripts/apps/movies/views/editor/movie-editor-cast-item.js
+++ public/javascripts/apps/movies/views/editor/movie-editor-cast-item.js
@@ -5,10 +5,28 @@ export default ModelView.extend({
   template,
   className: 'form-group',
   events: {
-    'click a': 'deleteActor'
+    'click a': 'deleteActor',
+    'change #avatar-input': 'handleAvatarSelect'
   },
   deleteActor( e ) {
     e.preventDefault();
     this.trigger('actor:delete', this.model);
+  },
+  handleAvatarSelect( e ) {
+    e.preventDefault();
+
+    const
+      img = this.$('#img-avatar'),
+      selectedFile = this.$('#avatar-input')[0].files[0],
+      fileReader = new FileReader();
+
+    fileReader.onload = function ( event ) {
+      const url = event.target.result;
+
+      img.attr('src', url);
+    };
+
+    fileReader.readAsDataURL( selectedFile );
+    this.trigger('avatar:select', selectedFile);
   }
 });
