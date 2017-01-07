2d00ad18f5d2a4270e78a75cdf0eebc837bde3c6
Adel Jojo <bigfanjs@gmail.com>
2017-01-07 21:56:03 +0000

    using ES6 promises instead of callbacks

--- public/javascripts/apps/movies/controllers/editor.js
+++ public/javascripts/apps/movies/controllers/editor.js
@@ -12,43 +12,53 @@ const
     App.notify();
     App.router.navigate('admin/dashboard', true);
   },
-  uploadCover = function (movie, callback) {
+  uploadCover = function (movie) {
     this.trigger('cover:upload:start');
 
-    movie.uploadCover(this.cover, {
-      progress: (length, uploaded, percent) => {
-        this.trigger(
-          'cover:upload:progress',
-          {length, uploaded, percent}
-        );
-      },
-      success: res => {
-        this.trigger('cover:upload:done');
-        callback( res );
-      },
-      error: err => {
-        this.trigger('cover:upload:fail');
-      }
+    const promise = new Promise((res, rej) => {
+      movie.uploadCover(this.cover, {
+        progress: (length, uploaded, percent) => {
+          this.trigger(
+            'cover:upload:progress',
+            {length, uploaded, percent}
+          );
+        },
+        success: res => {
+          this.trigger('cover:upload:done');
+          res( res );
+        },
+        error: err => {
+          this.trigger('cover:upload:fail');
+          rej( err );
+        }
+      });
     });
+
+    return promise;
   },
-  uploadAvatar = function (character, callback) {
+  uploadAvatar = function (character) {
     this.trigger('avatar:upload:start');
 
-    character.uploadAvatar(this.castItem, {
-      progress: (length, uploaded, percent) => {
-        this.trigger(
-          'avatar:upload:progress',
-          {length, uploaded, percent}
-        );
-      },
-      success: res => {
-        this.trigger('avatar:upload:done');
-        callback( res );
-      },
-      error: err => {
-        this.trigger('avatar:upload:fail');
-      }
+    const promise = new Promise((res, rej) => {
+      character.uploadAvatar(this.avatar, {
+        progress: (length, uploaded, percent) => {
+          this.trigger(
+            'avatar:upload:progress',
+            {length, uploaded, percent}
+          );
+        },
+        success: res => {
+          this.trigger('avatar:upload:done');
+          res( res );
+        },
+        error: err => {
+          this.trigger('avatar:upload:fail');
+          rej( err );
+        }
+      });      
     });
+
+    return promise;
   },
   handleCoverSelect = function ( cover ) {
     this.cover = cover;
@@ -59,20 +69,19 @@ const
   save = function ( movie ) {
     movie.save(null, {
       success: () => {
+        let promises = [];
+
         if (typeof this.cover !== null) {
-          uploadCover.call(this, movie, notify);
+          promises.push(uploadCover.call(this, movie));
           this.cover = null;
         }
 
-        if (typeof this.starringItem !== null) {
-          uploadAvatar.call(this, movie, notify);
-          this.starringItem = null;
+        if (typeof this.avatar !== null) {
+          promises.push(uploadAvatar.call(this, movie));
+          this.avatar = null;
         }
 
-        if (typeof this.castItem !== null) {
-          uploadAvatar.call(this, movie, notify);
-          this.castItem = null;
-        } 
+        Promise.all(promises).then(notify, notify);
       },
       error: () => {
         console.log('Failed saving movie to the server');
