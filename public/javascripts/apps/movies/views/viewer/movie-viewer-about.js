bf39b44102c865d94c007f9d94ee31b3efc3d29f
Adel Jojo <bigfanjs@gmail.com>
2017-01-12 22:10:35 +0000

    changing the regions method and adding new events to the movie viewer about view

--- public/javascripts/apps/movies/views/viewer/movie-viewer-about.js
+++ public/javascripts/apps/movies/views/viewer/movie-viewer-about.js
@@ -1,10 +1,34 @@
 import Layout from '../../../../lib/layout';
+import App from '../../../../app';
 import template from '../../templates/viewer/movie-viewer-about.pug';
 
 export default Layout.extend({
   template,
   className: 'panel panel-simple',
   regions: {
-    actors: '.actor-list-container'
-  }
+    cast: '.cast-list-container',
+    starring: '.starring-list-container',
+  },
+  events: {
+    'click #back': 'back',
+    'click #watch-movie': 'watchMovie',
+    'click #watch-trailer': 'watchTrailer',
+    'click #like': 'like',
+    'click #dislike': 'dislike'
+  },
+  back() {
+    App.router.navigate('movies', true);
+  },
+  watchMovie() {
+    // handle movie watching here.
+  },
+  watchTrailer() {
+    // handle trailer watching here.
+  },
+  like() {
+    this.trigger('movie:like', this.model);
+  },
+  dislike() {
+    this.trigger('movie:dislike', this.model);
+  },
 });
