2efd4f5d463813d382d887d94c10b85124c585bd
Adel Jojo <bigfanjs@gmail.com>
2017-01-13 09:13:24 +0000

    handling the movie model's embedded cast array in the viewer controller

--- public/javascripts/apps/movies/controllers/viewer.js
+++ public/javascripts/apps/movies/controllers/viewer.js
@@ -1,10 +1,26 @@
 /* Proxyquireify forces us to write CommonJS modules */
 
+// const
+//   Backbone = require('backbone'),
+//   MovieViewerLayout = require('../views/viewer/movie-viewer-layout'),
+//   MovieViewerWidget = require('../views/viewer/movie-viewer-widget'),
+//   MovieViewerAbout = require('../views/viewer/movie-viewer-about');
+
+import Backbone from 'backbone';
+import bind from 'lodash/bind';
+import MovieViewerLayout from '../views/viewer/movie-viewer-layout';
+import MovieViewerWidget from '../views/viewer/movie-viewer-widget';
+import MovieViewerAbout from '../views/viewer/movie-viewer-about';
+import MovieCharCollection from '../collections/movie-char-collection';
+import MovieCastView from '../views/viewer/movie-viewer-cast-view';
+
 const
-  Backbone = require('backbone'),
-  MovieViewerLayout = require('../views/viewer/movie-viewer-layout'),
-  MovieViewerWidget = require('../views/viewer/movie-viewer-widget'),
-  MovieViewerAbout = require('../views/viewer/movie-viewer-about');
+  movieLiked = function ( model ) {
+
+  },
+  movieDisliked = function ( model ) {
+
+  };
 
 module.exports = {
   setup: function ( options ) {
@@ -16,13 +32,31 @@ module.exports = {
   },
   view( movie ) {
     const
+      cast = movie.get('cast') || [],
+      starring = cast.filter(actor => actor.star);
+
+    this.castCollection = new MovieCharCollection( cast );
+    this.starringCollection = new MovieCharCollection( starring );
+
+    const
       layout = new MovieViewerLayout(),
       widget = new MovieViewerWidget({ model: movie }),
-      about = new MovieViewerAbout({ model: movie });
+      about = new MovieViewerAbout({ model: movie }),
+      castList = new MovieCastView({
+        collection: this.castCollection
+      }),
+      starringList = new MovieCastView({
+        collection: this.starringCollection
+      });
 
     this.region.show( layout );
     layout.getRegion('widget').show( widget );
     layout.getRegion('about').show( about );
+    about.getRegion('cast').show(castList, true);
+    about.getRegion('starring').show(starringList, true);
+
+    this.listenTo(about, 'movie:like', bind(movieLiked, this));
+    this.listenTo(about, 'movie:dislike', bind(movieDisliked, this));
   },
   destroy() {
     this.region.remove();
