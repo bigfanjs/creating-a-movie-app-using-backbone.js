69a8a4e69655332daf71c489863291963a3fe5ec
Adel Jojo <bigfanjs@gmail.com>
2017-01-13 09:59:45 +0000

    creating a movie viewer actor view

---
+++ public/javascripts/apps/movies/views/viewer/movie-viewer-actor.js
@@ -0,0 +1,7 @@
+import ModelView from '../../../../lib/model-view';
+import template from '../../templates/viewer/movie-viewer-actor.pug';
+
+export default ModelView.extend({
+  template,
+  tagName: 'tr'
+});
