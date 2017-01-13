ec7e837c52238d2d21e4eec296bab3411d7a284d
Adel Jojo <bigfanjs@gmail.com>
2017-01-13 09:22:24 +0000

    fixing a little typo in the nodemon gulp task

--- tasks/nodemon.js
+++ tasks/nodemon.js
@@ -15,7 +15,7 @@ export default function () {
       watch: [
         'server.js',
         'lib/**/*.js',
-        'routes/**/*/js',
+        'routes/**/*.js',
         'models/**/*.js',
         'views/**/*.jade'
       ]
