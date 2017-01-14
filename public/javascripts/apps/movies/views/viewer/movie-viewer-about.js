e8c9ddc654a834027dd7e30b1e3b8d47b611b154
Adel Jojo <bigfanjs@gmail.com>
2017-01-14 09:53:21 +0000

    updating the class name of the movie viewer about view

--- public/javascripts/apps/movies/views/viewer/movie-viewer-about.js
+++ public/javascripts/apps/movies/views/viewer/movie-viewer-about.js
@@ -4,7 +4,7 @@ import template from '../../templates/viewer/movie-viewer-about.pug';
 
 export default Layout.extend({
   template,
-  className: 'panel panel-simple',
+  className: 'col-xs-12 col-sm-8 col-md-9',
   regions: {
     cast: '.cast-list-container',
     starring: '.starring-list-container',
