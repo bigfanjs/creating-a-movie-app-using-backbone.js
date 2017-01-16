afbd30240052397d8b3e3ffb196c7cb854871978
Adel Jojo <bigfanjs@gmail.com>
2017-01-16 00:02:35 +0000

    formating the release date embedded object field using Backbone.Stickit[movie viwer about template]

--- public/javascripts/apps/movies/views/viewer/movie-viewer-about.js
+++ public/javascripts/apps/movies/views/viewer/movie-viewer-about.js
@@ -1,8 +1,24 @@
+import values from 'lodash/values';
 import Layout from '../../../../lib/layout';
 import App from '../../../../app';
 import template from '../../templates/viewer/movie-viewer-about.pug';
 
 export default Layout.extend({
+  initialize() {
+    const
+      attr = 'releaseDate',
+      date = Object.keys(this.model.get(attr));
+
+    this.bindings = {
+      '#release-date': {
+        observe: attr,
+        onGet: function ( obj ) {
+          const vals = values( obj );
+          return vals.join('/');
+        }
+      }
+    };
+  },
   template,
   className: 'col-xs-12 col-sm-8 col-md-9',
   regions: {
